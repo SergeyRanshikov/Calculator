5bf9864dcdef00a60b7b118441ac8f0cb4f3d08b
ranss@rambler.ru <ranss@rambler.ru>
2023-06-27 19:59:47 +0300

    init commit

---
+++ src/main/java/com/example/homework212/HomeWork212Application.java
@@ -0,0 +1,13 @@
+package com.example.homework212;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork212Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeWork212Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/homework212/HomeWork212ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework212;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork212ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
